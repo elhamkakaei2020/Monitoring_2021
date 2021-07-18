25577a8246cce9e5bba9e4e0cf86b97b6c58e4b4
elhamkakaei2020 <74149816+elhamkakaei2020@users.noreply.github.com>
2021-07-18 11:49:07 +0200

    Update R_codes exam2021.r

--- R_codes exam2021.r
+++ R_codes exam2021.r
@@ -24,6 +24,9 @@ swia2018 <- raster("c_gls_SWI10_201804111200_GLOBE_ASCAT_V3.1.1.nc")
 swia2019 <- raster("c_gls_SWI10_201904011200_GLOBE_ASCAT_V3.1.1.nc")
 swia2020 <- raster("c_gls_SWI10_202004111200_GLOBE_ASCAT_V3.1.1.nc")
 
+#to make list by list.files function to sure that they have been listed in the order
+swialist <- list.files(pattern="c_gls_SWI10")
+swialist 
 # to get plot of images 
 #In order to put the title above the plots Make use of the function: main=
 #To get information of image and its layers use function of brick 
@@ -73,22 +76,6 @@ plot (swia2020, main= " 2020 " )
 # changing color of plot by colorRampPalette
 cl <- colorRampPalette(c('red','orange','yellow'))(100)
 #using par function for having plots in one page 
-par(mfrow=c(4,4))
-plot(swia2007 , col=cl , main=2007 )
-plot(swia2008 , col=cl , main=2008 )
-plot(swia2009 , col=cl , main=2009 )
-plot(swia2010 , col=cl , main=2010 )
-plot(swia2011 , col=cl , main=2011 )
-plot(swia2012 , col=cl , main=2012 )
-plot(swia2013 , col=cl , main=2013 )
-plot(swia2014 , col=cl , main=2014 )
-plot(swia2015 , col=cl , main=2015 )
-plot(swia2016 , col=cl , main=2016 )
-plot(swia2017 , col=cl , main=2017 )
-plot(swia2018 , col=cl , main=2018 )
-plot(swia2019 , col=cl , main=2019 )
-plot(swia2020 , col=cl , main=2020 )
-
 #To make par  making 2 groups because 14 imagesin one page got too small
 par(mfrow=c(3,3))
 plot(swia2007 , col=cl , main=2007 )
@@ -107,14 +94,9 @@ plot(swia2018 , col=cl , main=2018 )
 plot(swia2019 , col=cl , main=2019 )
 plot(swia2020 , col=cl , main=2020 )
 
-
-
-#to make list by list.files function for getting stack 
-swialist <- list.files(pattern="c_gls_SWI10")
-swialist 
-#to make stack from all images 
-# SWIA <- stack(list_raster )
-SWIA <- stack(swia2007  , swia2008 , swia2009 , swia2010 ,swia2011 , swia2012 , swia2013 ,swia2014 , swia2015 ,swia2016 , swia2017 , swia2018 , swia2019 , swia2020 )
+#to make stack from all images in the list 
+# SWIA <- stack(list.files )
+SWIA <- stack(swialist )
 plot(SWIA, col=cl)
 # change color
 clb <- colorRampPalette(c('red','blue','yellow'))(100)
@@ -125,32 +107,7 @@ plotRGB(SWIA , red=swia2007 , green=swia2014 , blue=swia2020 , stretch="lin")
 
 #To find different part from 2007 to 2020
 # put cldif <- colorRampPalette(c('blue','yellow','red'))(100)  as a color of plots 
-difA <- swia2007 - swia2009 
 cldif <- colorRampPalette(c('blue','yellow','red'))(100)  
-plot(difA, col=cldif , main= "2007 - 2009" )
-difB <- swia2009 - swia2011
-plot(difB, col=cldif , main= "2009 - 2011" )
-difC <- swia2011 - swia2013
-plot(difC, col=cldif , main= "2011 - 2013" )
-difD <- swia2013 - swia2015
-plot(difD, col=cldif , main= "2013 - 2015" )
-difE <- swia2015 - swia2017
-plot(difE, col=cldif , main= "2015 - 2017" )
-difF <- swia2017 - swia2019
-plot(difF, col=cldif , main= "2017 - 2019" )
-difG <- swia2019 - swia2020
-plot(difG, col=cldif , main= "2019 - 2020" )
-# using par function 
-par(mfrow=c(3,3))
-plot(difA, col=cldif , main= "2007 - 2009" )
-plot(difB, col=cldif , main= "2009 - 2011" )
-plot(difC, col=cldif , main= "2011 - 2013" )
-plot(difD, col=cldif , main= "2013 - 2015" )
-plot(difE, col=cldif , main= "2015 - 2017" )
-plot(difF, col=cldif , main= "2017 - 2019" )
-plot(difG, col=cldif , main= "2019 - 2020" )
-
-#to make crop from the location there is different 
 # putting different names to plot that obtain by getting different  
 difa <- swia2007 - swia2011 
 plot(difa, col=cldif , main= "2007 - 2011" )
@@ -158,6 +115,7 @@ difb <- swia2011 - swia2015
 plot(difb, col=cldif , main= "2011 - 2015" )
 difc <- swia2015 - swia2020 
 plot(difc, col=cldif , main= "2015 - 2020" )
+#to make crop from the location there is different 
 #to make crop for north Europ and ASia
 ext <- c(-50,100 ,40,100)  # xmin xmax ymin ymax
 difa_north <- crop(difa, ext)
